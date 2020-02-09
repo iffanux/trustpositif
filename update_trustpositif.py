79ff369a414a328d81683446ef5df8ad8cbdf9d1
Djan Iffan <djan.iffan@gmail.com>
2020-02-09 15:26:32 +0700

    Update update_trustpositif.py

--- update_trustpositif.py
+++ update_trustpositif.py
@@ -2,7 +2,7 @@
 #! Updating database trustpositif kominfo
 #! Prinsip kerjanya meredirect domain2 ke CNAME yang kita punya
 #! Cara Kerjanya adalah mengunduh database truspositif dan menyimpannya dalam domains.txt file,
-#! Menambahkan SOA Header dan merubah file ekstensi ke rpz
+#! Menambahkan SOA Header dan mengubah file ekstensi ke rpz
 #! Menjalankan script : python3 update_trustpositif.py
 #! Script ini bebas digunakan dan modifikasi | djan.iffan@iforte.co.id |
 import ssl
